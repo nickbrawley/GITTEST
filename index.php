8a59d5bc4848c2e269214c9b00801802cfc73d3e
James Gailey <jrgailey@gmail.com>
2012-08-27 15:53:46 -0500

    Revert "Changed title to James"
    This reverts commit 1b2c8b07a0bda7db26e295efef7a92b4f0af5a6b.

--- index.php
+++ index.php
@@ -2,11 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<<<<<<< HEAD
 <title>Nick</title>
-=======
-<title>James</title>
->>>>>>> Changed title to James
 </head>
 
 <body>
